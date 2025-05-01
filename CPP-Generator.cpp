fd1bdc261476a8339af329533feb5ac97bb8e6f7
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-05-01 13:07:08 +1000

    Add project files.

---
+++ CPP-Generator.cpp
@@ -0,0 +1,12 @@
+﻿// CPP-Generator.cpp : Defines the entry point for the application.
+//
+
+#include "CPP-Generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
