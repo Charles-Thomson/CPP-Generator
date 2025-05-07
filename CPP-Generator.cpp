57b649848c57f6c67435d33ec23c0f046c9e7252
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2025-05-07 12:33:37 +1000

    Defined promise_type for Generator - not fully built out for requiered use case

--- CPP-Generator.cpp
+++ CPP-Generator.cpp
@@ -8,8 +8,32 @@
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
+class Generator {
+public:
+
+	// Define the promise type struct
+	struct promise_type {
+		// Value returned to caller. yieled by co_yield
+		int current_value;
+
+		Generator get_return_object() {
+			return Generator{ coroutine_handle<promise_type>::from_promise(*this) };
+		
+		}
+
+		
+
+
+
+
+	};
+
+	using handle_type = coroutine_handle<promise_type>;
+
+	explicit Generator(handle_type h) : handle(h) {}
+
+private:
+	handle_type handle;
+
+
+};
